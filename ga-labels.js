e6737e2ec3387890061fd3c57d499442627c032c
Tom Speak <thomas.speak@googlemail.com>
2013-06-19 12:55:09 +0100

    initial commit

--- ga-labels.js
+++ ga-labels.js
@@ -1,26 +1,26 @@
-/* @TODO
-		- add attachEvent
-		- implement universal analytics
-	*/
-var doc = document.documentElement;
+$(document).click(function(e) {
+	var clickedElement = $(e.target);
 
-if(doc.addEventListener) {
+	if(clickedElement.hasClass('ga-track-event')) {
 
-	doc.addEventListener('click', function (e) {
-	    if (e.target.classList.contains('ga-track-event')) {
-	    	var category = e.target.getAttribute('data-ga-cat');
-	    	var action = e.target.getAttribute('data-ga-act');
-	    	var label = e.target.getAttribute('data-ga-lab');
-		
-	    	if(label) {
-	    		_gaq.push(['_trackEvent', category, action, label]);
-	    	} else { 
-	    		_gaq.push(['_trackEvent', category, action]);
-	    	}
-	    } 
-	});
-} else if(doc.attachEvent) {
-	doc.attachEvent()
-} else {
-	console.log("old as hell bru");
-}
+		var category = clickedElement.attr('data-ga-cat');
+    	var action = clickedElement.attr('data-ga-act');
+    	var label = clickedElement.attr('data-ga-lab');
+    	var value = clickedElement.attr('data-ga-val');
+    	var nonInteraction = clickedElement.attr('data-ga-int');
+
+    	if(typeof(nonInteraction) === 'undefined') { var nonInteraction = true; }// DEFAULT TRUE = ignore the bounce rate calculation.
+
+    	if(label && value) {
+    		_gaq.push(['_trackEvent', category, action, label, value, nonInteraction]);
+    	} else if(label) {
+    		_gaq.push(['_trackEvent', category, action, label, '', nonInteraction]);
+    	} else if(value) {
+    		_gaq.push(['_trackEvent', category, action, '', value, nonInteraction]);
+    	} else { 
+    		_gaq.push(['_trackEvent', category, action, '', '', nonInteraction]);
+    	}
+
+    	console.log(['_trackEvent', category, action, '', '', nonInteraction]);
+	}
+});
