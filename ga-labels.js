73a344e9da5335c2b019f4c2dd6cf51de5844773
Thomas Speak <thomas.speak@gmail.com>
2013-06-19 14:00:00 +0200

    Update ga-labels.js

--- ga-labels.js
+++ ga-labels.js
@@ -3,7 +3,7 @@ $(document).click(function(e) {
 
 	if(clickedElement.hasClass('ga-track-event')) {
 
-				var category = clickedElement.attr('data-ga-cat');
+		var category = clickedElement.attr('data-ga-cat');
     		var action = clickedElement.attr('data-ga-act');
     		var label = clickedElement.attr('data-ga-lab');
     		var value = clickedElement.attr('data-ga-val');
@@ -11,14 +11,14 @@ $(document).click(function(e) {
 
     		if(typeof(nonInteraction) === 'undefined') { var nonInteraction = true; }// DEFAULT TRUE = ignore the bounce rate calculation.
 
-    	if(label && value) {
-    		_gaq.push(['_trackEvent', category, action, label, value, nonInteraction]);
-    	} else if(label) {
-    		_gaq.push(['_trackEvent', category, action, label, '', nonInteraction]);
-    	} else if(value) {
-    		_gaq.push(['_trackEvent', category, action, '', value, nonInteraction]);
-    	} else { 
-    		_gaq.push(['_trackEvent', category, action, '', '', nonInteraction]);
-    	}
+	    	if(label && value) {
+	    		_gaq.push(['_trackEvent', category, action, label, value, nonInteraction]);
+	    	} else if(label) {
+	    		_gaq.push(['_trackEvent', category, action, label, '', nonInteraction]);
+	    	} else if(value) {
+	    		_gaq.push(['_trackEvent', category, action, '', value, nonInteraction]);
+	    	} else { 
+	    		_gaq.push(['_trackEvent', category, action, '', '', nonInteraction]);
+	    	}
 	}
 });
